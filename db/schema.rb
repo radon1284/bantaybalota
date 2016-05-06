3809b32bb95c1b031fe618e545d878439e6446ad
Prelang Builder <builder@prelang.com>
2016-05-06 23:31:45 +0800

    Executed 'rake db:migrate' (development and test) to migrate all generated migrations which created schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506152714) do
+ActiveRecord::Schema.define(version: 20160506152746) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -61,5 +61,42 @@ ActiveRecord::Schema.define(version: 20160506152714) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+
+  create_table "reports", force: true do |t|
+    t.integer  "user_id"
+    t.string   "precinct_number"
+    t.string   "ballot_number"
+    t.string   "vote_receipt_number"
+    t.string   "bei_name"
+    t.integer  "voted_president"
+    t.integer  "pcos_result_president"
+    t.integer  "voted_vice_president"
+    t.integer  "pcos_result_vice_president"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reports", ["user_id"], name: "index_reports_on_user_id"
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
