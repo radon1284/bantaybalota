1e6d60c458b5f9c9b10b1393579016d307bb544d
Prelang Builder <builder@prelang.com>
2016-05-06 15:27:58 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Report' to allow for create, update

--- app/admin/report.rb
+++ app/admin/report.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Report do
+  permit_params :precinct_number, :ballot_number, :vote_receipt_number, :bei_name, :voted_president, :pcos_result_president, :voted_vice_president, :pcos_result_vice_president
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
